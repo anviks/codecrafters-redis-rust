f59249aa57a140a4382d20fb8090ce19c8209bf5
Andreas Viks <117015417+anviks@users.noreply.github.com>
2026-04-04 15:39:05 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,33 @@
-use std::{
-    io::{Read, Write},
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
     net::TcpListener,
 };
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    for stream in listener.incoming() {
-        tokio::spawn(async {
-            match stream {
-                Ok(mut stream) => loop {
-                    let mut buf = [0; 512];
-                    match stream.read(&mut buf) {
-                        Ok(0) => break,
-                        Ok(_) => stream.write_all(b"+PONG\r\n").unwrap(),
-                        Err(_) => break,
+    loop {
+        match listener.accept().await {
+            Ok((mut stream, _)) => {
+                tokio::spawn(async move {
+                    loop {
+                        let mut buf = [0; 512];
+                        match stream.read(&mut buf).await {
+                            Ok(0) => break,
+                            Ok(_) => {
+                                if let Err(_) = stream.write_all(b"+PONG\r\n").await {
+                                    break;
+                                }
+                            }
+                            Err(_) => break,
+                        }
                     }
-                },
-                Err(e) => {
-                    println!("error: {}", e);
-                }
+                });
             }
-        });
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
     }
 }
