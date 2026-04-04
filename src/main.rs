cb7815b641d52ea3e049e73316e6112bd223ac7c
Andreas Viks <117015417+anviks@users.noreply.github.com>
2026-04-04 15:09:18 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                stream.write_all(b"+PONG\r\n").unwrap();
-            }
+            Ok(mut stream) => loop {
+                let mut buf = [0; 512];
+                match stream.read(&mut buf) {
+                    Ok(0) => break,
+                    Ok(_) => stream.write_all(b"+PUNG\r\n").unwrap(),
+                    Err(_) => break,
+                }
+            },
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
 }
-
