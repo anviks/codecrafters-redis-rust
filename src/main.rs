a6945e55a997195552314db888fd03de66268703
Andreas Viks <117015417+anviks@users.noreply.github.com>
2026-04-04 15:23:21 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,25 @@ use std::{
     net::TcpListener,
 };
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => loop {
-                let mut buf = [0; 512];
-                match stream.read(&mut buf) {
-                    Ok(0) => break,
-                    Ok(_) => stream.write_all(b"+PONG\r\n").unwrap(),
-                    Err(_) => break,
+        tokio::spawn(async {
+            match stream {
+                Ok(mut stream) => loop {
+                    let mut buf = [0; 512];
+                    match stream.read(&mut buf) {
+                        Ok(0) => break,
+                        Ok(_) => stream.write_all(b"+PONG\r\n").unwrap(),
+                        Err(_) => break,
+                    }
+                },
+                Err(e) => {
+                    println!("error: {}", e);
                 }
-            },
-            Err(e) => {
-                println!("error: {}", e);
             }
-        }
+        });
     }
 }
