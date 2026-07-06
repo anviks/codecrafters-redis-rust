1ce9a04b9c837179326c242f7aebd68be4ac4bcf
Andreas Viks <117015417+anviks@users.noreply.github.com>
2026-07-06 16:11:48 +0300

    Move RESP decoding to separate file

---
+++ src/decode.rs
@@ -0,0 +1,71 @@
+#[derive(Debug)]
+pub(crate) enum RESPValue {
+    SimpleString(String),
+    SimpleError(String),
+    Integer(i32),
+    BulkString(String),
+    Array(Vec<RESPValue>),
+    Null,
+}
+
+fn read_until_crlf<'a>(input: &'a [u8], i: &mut usize) -> &'a [u8] {
+    let start = *i;
+    while input[*i] != b'\r' {
+        *i += 1;
+    }
+    let slice = &input[start..*i];
+    *i += 2; // skip \r\n
+    slice
+}
+
+fn decode_value(input: &[u8], i: &mut usize) -> RESPValue {
+    let type_byte = input[*i];
+    *i += 1;
+
+    return match type_byte {
+        b'+' => RESPValue::SimpleString(
+            str::from_utf8(read_until_crlf(input, i))
+                .unwrap()
+                .to_string(),
+        ),
+        b'-' => RESPValue::SimpleError(
+            str::from_utf8(read_until_crlf(input, i))
+                .unwrap()
+                .to_string(),
+        ),
+        b':' => RESPValue::Integer(
+            str::from_utf8(read_until_crlf(input, i))
+                .unwrap()
+                .parse()
+                .unwrap(),
+        ),
+        b'$' => {
+            let count: i32 = str::from_utf8(read_until_crlf(input, i))
+                .unwrap()
+                .parse()
+                .unwrap();
+            if count == -1 {
+                return RESPValue::Null;
+            }
+            let slice = &input[*i..*i + count as usize];
+            *i += count as usize + 2;
+            RESPValue::BulkString(str::from_utf8(slice).unwrap().to_string())
+        }
+        b'*' => {
+            let count: i32 = str::from_utf8(read_until_crlf(input, i))
+                .unwrap()
+                .parse()
+                .unwrap();
+            if count == -1 {
+                return RESPValue::Null;
+            }
+            RESPValue::Array((0..count).map(|_| decode_value(input, i)).collect())
+        }
+        _ => panic!("unexpected byte: {}", type_byte),
+    };
+}
+
+pub fn decode(input: &[u8]) -> RESPValue {
+    let mut i = 0;
+    decode_value(input, &mut i)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,9 @@
-use std::char::from_digit;
-
+use crate::decode::{RESPValue, decode};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpListener,
 };
-
-#[derive(Debug)]
-enum RESPValue {
-    SimpleString(String),
-    Error(String),
-    Integer(i32),
-    BulkString(String),
-    Array(Vec<RESPValue>),
-    Null,
-}
-
-fn read_until_crlf<'a>(input: &'a [u8], i: &mut usize) -> &'a [u8] {
-    let start = *i;
-    while input[*i] != b'\r' {
-        *i += 1;
-    }
-    let slice = &input[start..*i];
-    *i += 2; // skip \r\n
-    slice
-}
-
-fn parse_resp(input: &[u8], i: &mut usize) -> RESPValue {
-    let type_byte = input[*i];
-    *i += 1;
-
-    return match type_byte {
-        b'+' => RESPValue::SimpleString(str::from_utf8(read_until_crlf(input, i)).unwrap().to_string()),
-        b'-' => RESPValue::Error(str::from_utf8(read_until_crlf(input, i)).unwrap().to_string()),
-        b':' => RESPValue::Integer(str::from_utf8(read_until_crlf(input, i)).unwrap().parse().unwrap()),
-        b'$' => {
-            let count: i32 = str::from_utf8(read_until_crlf(input, i)).unwrap().parse().unwrap();
-            if count == -1 { return RESPValue::Null; }
-            let slice = &input[*i..*i + count as usize];
-            *i += count as usize + 2;
-            RESPValue::BulkString(str::from_utf8(slice).unwrap().to_string())
-        }
-        b'*' => {
-            let count: i32 = str::from_utf8(read_until_crlf(input, i)).unwrap().parse().unwrap();
-            if count == -1 { return RESPValue::Null; }
-            RESPValue::Array((0..count).map(|_| parse_resp(input, i)).collect())
-        }
-        _ => panic!("unexpected byte: {}", type_byte),
-    };
-}
+mod decode;
 
 #[tokio::main]
 async fn main() {
@@ -62,8 +18,7 @@ async fn main() {
                         match stream.read(&mut buf).await {
                             Ok(0) => break,
                             Ok(_) => {
-                                let mut i: usize = 0;
-                                let parsed = parse_resp(&buf, &mut i);
+                                let parsed = decode(&buf);
                                 println!("{:?}", parsed);
                                 if let RESPValue::Array(arr) = parsed {
                                     if let RESPValue::BulkString(cmd) = &arr[0]
